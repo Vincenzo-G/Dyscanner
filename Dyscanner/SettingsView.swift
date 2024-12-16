6b6a2d54f5b85ea96e9e8ef9a766cb63acbd4d49
Vincenzo-G <vincenzogerelli@gmail.com>
2024-12-16 16:07:12 +0100

    UI improvements

---
+++ Dyscanner/SettingsView.swift
@@ -0,0 +1,11 @@
+// Placeholder for SettingsView
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .font(.title)
+            .foregroundColor(Color.primary)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(UIColor.systemBackground))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
